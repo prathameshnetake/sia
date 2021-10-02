de47df97173b5e0c6aec4bfd1e5d5f7f71079193
Prathamesh Netake <prathamesh.netake@KOG-PNetake.local>
2021-10-02 16:50:53 +0530

    chore: add button

---
+++ src/components/Button/button.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+    const test = (e: Event) => {
+        console.log(e.target)
+    }
+</script>
+
+<template>
+    <button class="d-flex ml-2 text-red-400 p-2 border-blue-300 border-2" @click="test">Test</button>
+</template>
--- src/modules/topbar/topbar.vue
+++ src/modules/topbar/topbar.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+    import SiaButton from "../../components/Button/button.vue"
+</script>
+
 <template>
     this is topbar
+    <sia-button></sia-button>
 </template>
